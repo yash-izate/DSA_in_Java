76f04a90a1f01979944d927574beaae9e624f055
Yash Izate <yashizate.softech@gmail.com>
2024-12-23 00:22:01 +0530

    update

--- Java_Basics/output.java
+++ Java_Basics/output.java
@@ -7,15 +7,15 @@ public class output {
     System.out.print("Hello World");
     System.out.println();
 
-    // escape character \n - print on new line after it
+    // \n - print new line after it
     System.out.print("Hello India \nNamaste Bharat");
     System.out.println();
 
-    // new line after printing when println is used
+    // println - new line after printing when it is used
     System.out.println("Hello India");
     System.out.println();
 
-    // printf allows us to format strings
+    // printf - it allows us to format strings
     System.out.printf("Hi, My name is %s. I am %d years old.", "Yash", 21);
     System.out.println();
 
