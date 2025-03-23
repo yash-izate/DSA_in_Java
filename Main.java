4a797b5e78fdfe73b7f73eac7a4df63d616aae74
Yash Izate <yashizate.softech@gmail.com>
2025-03-23 23:59:32 +0530

    update

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     int num1 = 5;
-    int num2 = 10;
+    int num2 = 15;
     int sum = num1 + num2;
     System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
   }
