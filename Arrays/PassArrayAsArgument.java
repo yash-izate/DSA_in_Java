c1df75a7389dfeef419f3f183a360671858d22ea
Yash Izate <yashizate.softech@gmail.com>
2025-03-24 19:55:01 +0530

    pass by value and pass by reference

--- Arrays/PassArrayAsArgument.java
+++ Arrays/PassArrayAsArgument.java
@@ -2,7 +2,11 @@ package Arrays;
 
 public class PassArrayAsArgument {
 
-    public static void update(int arr[]) {
+    // function to update pass by reference and pass by value
+    public static void update(int arr[], int nonChangable) {
+
+        nonChangable = 20;
+
         for (int i = 0; i < arr.length; i++) {
             arr[i] = arr[i] + 1;
         }
@@ -11,14 +15,32 @@ public class PassArrayAsArgument {
 
     public static void main(String[] args) {
 
+        int nonChangable = 10;
+
         int marks[] = { 21, 31, 41, 51, 61 };
 
-        update(marks);
+        /*
+         * In nonchangable a copy of this variable is sent to the function, therefore
+         * variable in main function is unchanged
+         */
+
+        /*
+         * in marks array , reference of memory address is passed therefore any changes
+         * in the memory location in function is reflected in main also
+         */
+
+        update(marks, nonChangable);
 
         // print our array
         for (int i : marks) {
             System.out.print(i + "  ");
         }
+
+        // new line
+        System.out.println();
+
+        // print pass by value (non-changable)
+        System.out.println(nonChangable);
 
     }
 
