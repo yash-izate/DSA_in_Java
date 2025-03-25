d2e5eaccd2cb6e1cc9a2d18628e7b64f676fb1c9
Yash Izate <yashizate.softech@gmail.com>
2025-03-25 22:55:34 +0530

    update

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -19,7 +19,7 @@ public class LinearSearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int array[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+        int array[] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 30, 40, 50 };
 
         System.out.print("Enter no. to find in array: ");
         int key = sc.nextInt();
