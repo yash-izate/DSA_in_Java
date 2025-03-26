2481ee6ac83d164709c06b1eb069b47ddee3f7eb
Yash Izate <yashizate.softech@gmail.com>
2025-03-26 22:52:28 +0530

    add Binary search

--- Arrays/BinarySearch.java
+++ Arrays/BinarySearch.java
@@ -1,5 +1,43 @@
 package Arrays;
 
 public class BinarySearch {
-    
+
+    public static int binarySearch(int numArray[], int key) {
+        int start = 0;
+        int end = numArray.length;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            // comparisons
+            if (numArray[mid] == key) {
+
+                return mid;
+            }
+
+            // search right hand side
+            if (numArray[mid] < key) {
+
+                start = mid + 1;
+
+            } else {
+
+                end = mid - 1;
+            }
+
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int numbers[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
+        int key = 6;
+
+        int idx = binarySearch(numbers, key);
+
+        System.out.println("Index for key is: " + idx);
+
+    }
 }
