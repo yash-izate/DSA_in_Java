2da0649db8191dfca90d45b0be52ddadb52e00df
Yash Izate <yashizate.softech@gmail.com>
2025-04-06 18:48:47 +0530

    Add Kadanes max subarray sum

--- Arrays/MaxSubArraySum.java
+++ Arrays/MaxSubArraySum.java
@@ -32,9 +32,28 @@ public class MaxSubArraySum {
         System.out.println("Max Sum: " + maxSum);
     }
 
-    public static void main(String[] args) {
-        int numbers[] = { 2, -3, 4, 5, 6, 7, 8, -9, 10 };
+    public static void Kadanes(int numbers[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
 
-        maxSubArraySum(numbers);
+        for (int i = 0; i < numbers.length; i++) {
+            currSum += numbers[i];
+
+            if (currSum < 0) {
+                currSum = 0;
+            }
+
+            maxSum = Math.max(currSum, maxSum);
+        }
+
+        System.out.println("Max Subarray sum : " + maxSum);
+
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+
+        // maxSubArraySum(numbers);
+        Kadanes(numbers);
     }
 }
